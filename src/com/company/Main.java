4f95125e6d3824f60ca96c04996b09f7f7c9003b
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-15 21:00:39 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        tuna tunaObject = new tuna();
+        potpie potObject = new potpie();
+
+        tunaObject.eat();
+        potObject.eat();
+    }
+}
---
+++ src/com/company/tuna.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class tuna extends food {
+    public void eat(){
+        System.out.println("NEW METHOD");
+    }
+
+}
